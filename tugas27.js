6d72bae99340e3f47c2562aa2d47197accc8fa6a
Muhammad Rizwan <muhammad.rizwan76146@gmail.com>
2019-10-26 00:45:38 +0700

    tugas 27 js, Muhammad Rizwan

---
+++ tugas27.js
@@ -0,0 +1,13 @@
+/* Tugas 27 Javascript */
+// Muhammad Rizwan
+
+function tugasDuapuluhTujuh(){
+    var a = ["No","rumah saya",30,"A"];
+    var b = ["Jalan pangeran Tirtayasa No",70];
+
+    console.log(a.join(" "));
+    console.log(b.join(" "));
+
+}
+
+tugasDuapuluhTujuh();
